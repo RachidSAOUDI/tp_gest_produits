605abcead3eab0e022be858445516802164c1b8e
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-22 19:00:11 +0100

    initial commit

---
+++ src/main/java/ma/usmba/tp_gest_produits/TpGestProduitsApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.tp_gest_produits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGestProduitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGestProduitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/tp_gest_produits/TpGestProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.tp_gest_produits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGestProduitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
